6bf3d3e5e6a7825bc4ec314f8cec9469d25bab1b
Vadim Gringauz <vadim.grin28@gmail.com>
2022-11-10 21:49:11 +0200

    Milestone 5 v1.0
    Complete
    + add-button appears only when there is input

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -11,14 +11,16 @@ export default function AddButton (props) {
 
   return (
     <>
-      <button
-        type='button'
-        className='btn btn-secondary p-0 fs-3 fw-bolder rounded-circle'
-        onClick={handleClick}
-        style={{ width: '50px', height: '50px' }}
-      >
-        +
-      </button>
+      {text && (
+        <button
+          type='button'
+          className='btn btn-secondary p-0 fs-3 fw-bolder rounded-circle'
+          onClick={handleClick}
+          style={{ width: '50px', height: '50px' }}
+        >
+          +
+        </button>
+      )}
     </>
   )
 }
