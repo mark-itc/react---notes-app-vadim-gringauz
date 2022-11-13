5dfc583e8d6e65a3e762269515b70f4c488a8bcf
Vadim Gringauz <vadim.grin28@gmail.com>
2022-11-14 00:56:33 +0200

    Milestone 8 v0.1
    + Localforage works
    + ADD: save edit button disabled when no text to save

--- src/components/SaveEditButton.jsx
+++ src/components/SaveEditButton.jsx
@@ -12,7 +12,7 @@ export default function SaveEditButton (props) {
 
   return (
     <>
-      <Button variant='primary' onClick={handleClick}>
+      <Button variant='primary' onClick={handleClick} disabled={newText === ''}>
         Save & Close
       </Button>
     </>
